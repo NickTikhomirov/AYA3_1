1f757aed1992d218f1be25d791649b78541b8b70
NickTikhomirov <nicktikhomirov02@gmail.com>
2019-12-09 18:16:18 +0300

    no more whitespace check))

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,5 +2,5 @@
 set -e
 
 files=`find . -name "*.cpp" | grep -v "./tools/*"`
-filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/line_length,-whitespace/comments,-build/include_order
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/line_length,-whitespace/comments,-build/include_order,-whitespace/tab
 echo $files | xargs cpplint --filter=$filter
